acc6593b3cbcc82fee8f654791bde879a343952f
gavin <v_zhangtao15@tal.com>
2025-05-16 15:48:26 +0800

    demo

---
+++ mathutil/go.mod
@@ -0,0 +1,3 @@
+module github.com/zhtfirst/go_common_tool/mathutil
+
+go 1.20
---
+++ mathutil/mathutil.go
@@ -0,0 +1,6 @@
+package mathutil
+
+// Add takes two integers and returns their sum.
+func Add(a int, b int) int {
+	return a + b
+}
